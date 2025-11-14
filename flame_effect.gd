f41b08a1db9bf08bc34df66235268422c6503b50
exclusiveaudio03-maker <exclusiveaudio03@gmail.com>
2025-11-14 11:06:38 +0000

    Delete flame_effect.gd

--- flame_effect.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-func play(pos):
-    var scene_path = 'res://scenes/vfx/fire_vent_vfx.tscn'
-    if ResourceLoader.exists(scene_path):
-        var s = load(scene_path).instantiate()
-        s.translation = pos
-        get_tree().get_current_scene().add_child(s)
-    else:
-        print('Flame burst at', pos)
